a1c3e680fb88720d3b3e6e0f671e3875457290bc
Felipe-53 <felipeandresb97@gmail.com>
2023-04-02 16:38:33 -0300

    remove test script from implementation file

--- src/dijkstra/priorityQueue/index.ts
+++ src/dijkstra/priorityQueue/index.ts
@@ -105,21 +105,3 @@ class ProgressTracker {
     this.closedNodes.add(label);
   }
 }
-
-const graph = new Graph();
-graph.addVertex("A");
-graph.addVertex("B");
-graph.addVertex("C");
-graph.addVertex("D");
-graph.addVertex("E");
-
-graph.addEdge("A", "B", 6);
-graph.addEdge("A", "D", 1);
-graph.addEdge("B", "D", 2);
-graph.addEdge("B", "E", 2);
-graph.addEdge("B", "C", 5);
-graph.addEdge("C", "E", 5);
-graph.addEdge("D", "E", 1);
-
-const results = shortestPathFinder(graph, "A");
-console.log(results);
